85cd3bebe3fb9869016cd8a108e04da3b47b16ea
Kristijan Burnik <kristijanburnik@gmail.com>
2018-11-20 17:56:20 +0100

    Update install script.

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source ${DIR}/.env
 
-git config --global user.name ${GIT_USER_NAME}
-git config --global user.email ${GIT_USER_EMAIL}
-
 if ! which python; then
   apt-get update
   apt-get install -y python-minimal
@@ -21,4 +18,3 @@ fi
 if ! pip list | grep locustio; then
  pip install locustio
 fi
-
